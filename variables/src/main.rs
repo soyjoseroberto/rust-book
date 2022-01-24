1e9f19b68a6daa43a7b854ffa2663af52ab36425
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-23 22:31:22 -0500

    Good use case for shadowing a variable.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // Example of shadowing where you avoid using spaces_str and spaces_num as var names
+    let spaces = "   ";
+    let spaces = spaces.len();
+    println!("The number of spaces is {}", spaces);
+    
 }
