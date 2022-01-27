1ff74cc3d36ad63bf2905c382bc0d3f44da81cb8
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-26 22:38:53 -0500

    Started ownership chapter.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -22,7 +22,11 @@ fn main() {
         println!("{}!", number);
     }
     println!("LIFTOFF!!!");
-    
-    
-    
+
+    // Chapter 4: Ownership
+    let mut s = String::from("hello");
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{}", s); // This will print `hello, world!`
 }
