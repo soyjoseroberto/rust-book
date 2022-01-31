b950fa29860661bbf5c2b6566f6cfcaa049f70d6
soyjoseroberto <jrodriguez18@gmail.com>
2022-01-30 23:16:58 -0500

    Control flow with if else.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -23,6 +23,19 @@ fn main() {
     }
     println!("LIFTOFF!!!");
 
+    // Control flow with if else
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
     // Chapter 4: Ownership
     let mut s = String::from("hello");
 
